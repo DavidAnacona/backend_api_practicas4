24fb2faec078e86c6c42b66080e34984ce2bdfea
DavidAnacona <danaconac@ucentral.edu.co>
2023-11-25 09:45:03 -0500

    puerto

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const especialistaRoutes = require('./routes/especialista.routes.js')
 const pacienteRoutes = require('./routes/paciente.routes.js')
 const examenRoutes = require("./routes/examen.routes.js")
 const fotoRoutes = require("./routes/imagen.routes.js")
+const PORT = process.env.PORT || 3001; 
 
 app.use(cors());
 app.use(express.json())
@@ -21,6 +22,6 @@ app.get('/', (req, res) => {
     res.send('Servidor en funcionamieto')
 })
 
-app.listen(3001, () => {
-    console.log('Server UP running in http://localhost:3001')
-})
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server UP running on port ${PORT}`);
+  });
